989a4e5978d300ecce7dedb7bd88d5b2ef293e51
Agilulf <1203577977@qq.com>
2019-11-24 23:36:25 +0800

    v1.1 finished

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -15,30 +15,55 @@ def main():
         4: ' ', 5: ' ', 6: ' ',
         1: ' ', 2: ' ', 3: ' '
     }#dic
-    init_list = {1,2,3,4,5,6,7,8,9}
+    #init_list = [1,2,3,4,5,6,7,8,9]
+    init_set = {1,2,3,4,5,6,7,8,9}
     begin = True
     while begin:
         curr_board = init_board.copy() #Python 直接赋值、浅拷贝和深度拷贝
-        curr_list = init_list.copy()
+        curr_set = init_set.copy()
+        player1_set = set()
+        player2_set = set()
+        player1win = 0
+        player2win = 0
+        winpattern= [{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9},{3,5,7}]
         begin = False
         turn = 'x'
         counter = 0
         os.system('cls')
         print_board(curr_board)
-        while counter < 9:
+        while curr_set and player1win == 0 and player2win == 0:
             if turn == 'x':
                 move = input('轮到%s走棋, 请输入位置: ' % turn)
                 move = int(move)
-                if curr_board[move] == ' ':
-                    curr_board[move] = turn
-                    curr_list.remove(move)      
+                while curr_board[move] != ' ':
+                    move = input('轮到%s走棋, 请输入位置: ' % turn)
+                    move = int(move)
                 curr_board[move] = turn
-                if turn == 'x':
-                    turn = 'o'
-                else:
-                    turn = 'x'
+                curr_set.remove(move)
+                player1_set.add(move)
+            else:
+                from random import choice
+                move = choice(list(curr_set))
+                curr_board[move] = turn
+                curr_set.remove(move)
+                player2_set.add(move)
+            if turn == 'x':
+                turn = 'o'
+            else:
+                turn = 'x'
+            for pt in winpattern:
+                if pt.issubset(player1_set):#don't forget :
+                    player1win = 1
+                if pt.issubset(player2_set):
+                    player2win = 1
             os.system('cls')
-            print_board(curr_board)
+            print_board(curr_board)   
+        if player1win:
+            print('You win') 
+        elif player2win:
+            print('You lost') 
+        else:
+            print('draw') 
         choice = input('再玩一局?(yes|no)')
         begin = choice == 'yes'
 
